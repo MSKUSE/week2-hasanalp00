0ae689cd5ff9d26e6d82728e319959518d8c6975
hasanalp00 <hasanalp1000@gmail.com>
2025-02-27 13:16:29 +0300

    Add files via upload

--- Me .java
+++ Me .java
@@ -1,3 +1,9 @@
-public class Me {
-    
+public class Me{
+    public static void main(String[] args) {
+        int val1=38;
+        float val2=38.8f;
+        char al="H";
+        boolean val3= true;
+        System.out.println(val1+val2);
+    }
 }
